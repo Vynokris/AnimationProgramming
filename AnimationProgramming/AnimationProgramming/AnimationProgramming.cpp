e56066a8b1803b3ef486b232bdc2a06e0d24502a
Vynokris <vynokris@gmail.com>
2022-12-09 15:08:30 +0100

    :tada: Added project source

---
+++ AnimationProgramming/AnimationProgramming/AnimationProgramming.cpp
@@ -0,0 +1,47 @@
+// AnimationProgramming.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "Engine.h"
+#include "Simulation.h"
+
+class CSimulation : public ISimulation
+{
+	virtual void Init() override
+	{
+		int spine01 =	GetSkeletonBoneIndex("spine_01");
+		int spineParent = GetSkeletonBoneParentIndex(spine01);
+		const char* spineParentName = GetSkeletonBoneName(spineParent);
+
+		float posX, posY, posZ, quatW, quatX, quatY, quatZ;
+		size_t keyCount = GetAnimKeyCount("ThirdPersonWalk.anim");
+		GetAnimLocalBoneTransform("ThirdPersonWalk.anim", spineParent, keyCount / 2, posX, posY, posZ, quatW, quatX, quatY, quatZ);
+		
+		printf("Spine parent bone : %s\n", spineParentName);
+		printf("Anim key count : %ld\n", keyCount);
+		printf("Anim key : pos(%.2f,%.2f,%.2f) rotation quat(%.10f,%.10f,%.10f,%.10f)\n", posX, posY, posZ, quatW, quatX, quatY, quatZ);
+	}
+
+	virtual void Update(float frameTime) override
+	{
+		// X axis
+		DrawLine(0, 0, 0, 100, 0, 0, 1, 0, 0);
+
+		// Y axis
+		DrawLine(0, 0, 0, 0, 100, 0, 0, 1, 0);
+
+		// Z axis
+		DrawLine(0, 0, 0, 0, 0, 100, 0, 0, 1);
+
+	}
+};
+
+int main()
+{
+	CSimulation simulation;
+	Run(&simulation, 1400, 800);
+
+    return 0;
+}
+
