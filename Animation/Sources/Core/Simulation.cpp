ad63a53029a9bec481df43f7a5823741396eb766
Vynokris <vynokris@gmail.com>
2022-12-16 21:20:57 +0100

    :pencil2: Changes to ui

--- Animation/Sources/Core/Simulation.cpp
+++ Animation/Sources/Core/Simulation.cpp
@@ -87,14 +87,14 @@ void CSimulation::DrawGizmo(const Vector3& offset, const float& size) const
 
 void CSimulation::ShowImGui(const float& deltaTime)
 {
-    if (ImGui::Begin("Stats"))
+    if (ImGui::Begin("Stats", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize))
     {
         ImGui::Text("FPS: %d", roundInt(1/deltaTime));
         ImGui::Text("Delta Time: %f", deltaTime);
     }
     ImGui::End();
 
-    if (ImGui::Begin("Animations", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
+    if (ImGui::Begin("Animations", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysAutoResize))
     {
         Animator& animator = skeleton.GetAnimator();
         const bool isAnimating = animator.IsAnimating();
@@ -102,13 +102,17 @@ void CSimulation::ShowImGui(const float& deltaTime)
         std::unordered_map<std::string, Animation*>& animations = animator.GetAllAnimations();
 
         // Current animation dropdown menu selection.
-        ImGui::Text("Current animation:");
+        ImGui::Unindent(4);
+        ImGui::BulletText("Current animation:");
+        ImGui::Indent(4);
+        ImGui::Indent(16);
+        ImGui::SetNextItemWidth(201);
         if (ImGui::BeginCombo("##curAnimInput", (isAnimating ? curAnim->nameNoExtension.c_str() : "none")))
         {
             // No animation.
             {
                 const bool isSelected = !isAnimating;
-                if (ImGui::Selectable("none", isSelected)) {
+                if (ImGui::Selectable("none", isSelected) && !animator.IsTransitioning()) {
                     animator.SetCurrentAnimation("");
                 }
                 if (isSelected) {
@@ -120,7 +124,7 @@ void CSimulation::ShowImGui(const float& deltaTime)
             for (auto& [name, anim] : animations)
             {
                 const bool isSelected = (name == (isAnimating ? curAnim->name : ""));
-                if (ImGui::Selectable(anim->nameNoExtension.c_str(), isSelected)) {
+                if (ImGui::Selectable(anim->nameNoExtension.c_str(), isSelected) && !animator.IsTransitioning()) {
                     animator.SetCurrentAnimation(name);
                 }
                 if (isSelected) {
@@ -129,29 +133,52 @@ void CSimulation::ShowImGui(const float& deltaTime)
             }
             ImGui::EndCombo();
         }
+        ImGui::Unindent(16);
         ImGui::NewLine();
 
         // Start transition button.
-        static float transitionDuration = 2.f;
-        if (ImGui::Button("Start transition") && animator.IsAnimating() && !animator.IsTransitioning())
+        if (animator.IsAnimating())
         {
-            const std::string curAnimName = skeleton.GetAnimator().GetCurrentAnimationName();
-            std::string destAnimName = WALK_ANIMATION;
-            if (curAnimName == WALK_ANIMATION) destAnimName = RUN_ANIMATION;
-            skeleton.GetAnimator().StartTransition(destAnimName, transitionDuration);
-        }
-        ImGui::SameLine(); ImGui::SetNextItemWidth(29);
-        if (ImGui::DragFloat("Duration", &transitionDuration, 0.01f, 0.5f, 9.9f, "%.1f")) {
-            transitionDuration = clamp(transitionDuration, 0.1f, 9.9f);
+            ImGui::Unindent(4);
+            ImGui::BulletText("Animation transition:");
+            ImGui::Indent(4);
+            ImGui::Indent(16);
+            if (!animator.IsTransitioning())
+            {
+                static float transitionDuration = 2.f;
+                if (ImGui::Button("Start") && animator.IsAnimating() && !animator.IsTransitioning())
+                {
+                    const std::string curAnimName = skeleton.GetAnimator().GetCurrentAnimationName();
+                    std::string destAnimName = WALK_ANIMATION;
+                    if (curAnimName == WALK_ANIMATION) destAnimName = RUN_ANIMATION;
+                    skeleton.GetAnimator().StartTransition(destAnimName, transitionDuration);
+                }
+                ImGui::SameLine(); ImGui::SetNextItemWidth(29);
+                if (ImGui::DragFloat("Duration", &transitionDuration, 0.01f, 0.5f, 9.9f, "%.1f")) {
+                    transitionDuration = clamp(transitionDuration, 0.1f, 9.9f);
+                }
+            }
+            else
+            {
+                ImGui::AlignTextToFramePadding();
+                ImGui::Text("In transition (%d%%)", roundInt(animator.GetTransitionCompletion() * 100));
+            }
+            ImGui::Unindent(16);
+            ImGui::NewLine();
         }
         
         // Animation collapsing headers.
+        ImGui::Unindent(4);
+        ImGui::BulletText("Animation parameters:");
+        ImGui::Indent(4);
+        ImGui::Indent(19);
         for (auto& [name, anim] : animations)
         {
             ImGui::PushID(name.c_str());
             if (ImGui::CollapsingHeader(anim->nameNoExtension.c_str()))
             {
-            
+                ImGui::Indent(23);
+
                 // Current key frame.
                 ImGui::AlignTextToFramePadding();
                 ImGui::Text("Current keyframe:");
@@ -181,9 +208,12 @@ void CSimulation::ShowImGui(const float& deltaTime)
                 if (ImGui::DragFloat("Speed", &animSpeed, 0.1f, 0.5f, 99.f, "%.0f") && !animator.IsTransitioning()) {
                     anim->keyframeDuration = 1 / clamp(animSpeed, 0.5f, 99.f);
                 }
+
+                ImGui::Unindent(23);
             }
             ImGui::PopID();
         }
+        ImGui::Unindent(19);
     }
     ImGui::End();
 }
